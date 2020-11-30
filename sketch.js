1f6f3d831b7f82abfe5268d40b1810faf9b74142
Gargi <gargi.chaturvedi@outlook.com>
2020-11-30 17:00:45 +0530

    Constraint was added in these files

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +17,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -29,13 +31,14 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
+    constrainedLog = new Log(230, 180, 80, PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body, constrainedLog.body);
 }
 
 function draw(){
@@ -54,6 +57,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    constrainedLog.display();
 
     box5.display();
     log4.display();
@@ -61,4 +65,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
 }
